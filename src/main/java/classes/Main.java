6877e0727ddfe98b4efa721cb1fd6456881578fd
zeeman0306 <zeeman0306@gmail.com>
2018-07-01 13:12:52 -0400

    added all files

---
+++ src/main/java/classes/Main.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        System.out.println(5 + 6);
+
+        System.out.println( 5 > 6 );
+    }
+}
